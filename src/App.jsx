4420f4469d48390f94ccaee23c1d8b1ebb6ba17e
luis revilla <lrev4766@gmail.com>
2024-09-25 23:40:40 -0500

    restructure layuot

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,47 @@
-import React from "react"; // Import React (this is only necessary if you're using React 17 or earlier without the new JSX transform)
-// import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./sass/main.scss";
-import CanvasDots from "./components/heroCanvas";
-import Hero from "./components/Hero"; // Import the Hero component
+import Hero from "./components/Hero";
 import NavBar from "./components/Header";
 import CanvasDotsBg from "./components/bgCanvas";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import ContactForm from "./components/Contact";
 import Footer from "./components/Footer";
+import AllProjects from "./components/AllProjects"; // Import the AllProjects component
 
 function App() {
   return (
-    <div>
-      {/* <CanvasDots /> */}
-      <Hero />
-      <NavBar />
-      <CanvasDotsBg />
-      <About />
-      <Projects />
-      <ContactForm />
-
-      <Footer />
-
-      {/* You can add more components here as your application develops */}
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <NavBar />
+              <CanvasDotsBg />
+              <About />
+              <Projects />
+              <ContactForm />
+              <Footer />
+            </>
+          }
+        />
+        {/* You can add more components here as your application develops */}
+        <Route
+          path="/all-projects"
+          element={
+            <>
+              <NavBar />
+              <CanvasDotsBg />
+              <AllProjects />
+              <Footer />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
