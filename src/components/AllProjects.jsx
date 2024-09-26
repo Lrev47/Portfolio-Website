35339b43acf3e3061f4783a73bc71e94a656ad1c
luis revilla <lrev4766@gmail.com>
2024-09-25 23:40:18 -0500

    add basic all project page

---
+++ src/components/AllProjects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AllProjects = () => {
+  return (
+    <section className="all-projects">
+      <h2 className="all-projects__heading">All Projects</h2>
+      <p className="all-projects__placeholder">PLACEHOLDER</p>
+    </section>
+  );
+};
+
+export default AllProjects;
