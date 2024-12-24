8e0e8f2a40ee830146a3f0d0ba4aa912e708e5ff
luis revilla <lrev4766@gmail.com>
2024-12-23 20:25:13 -0600

    added scoll to top helper function

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 
+const handleScrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth", // for a smooth scroll animation
+  });
+};
+
 function Footer() {
   return (
     <footer className="footer">
-      <a href="#hero">
-        <div className="return-home">
-          <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            width="100%"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="#fafafa"
-              d="M17.707 10.293l-5-5c-0.391-0.391-1.024-0.391-1.414 0l-5 5c-0.391 0.391-0.391 1.024 0 1.414s1.024 0.391 1.414 0l4.293-4.293 4.293 4.293c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414zM17.707 17.293l-5-5c-0.391-0.391-1.024-0.391-1.414 0l-5 5c-0.391 0.391-0.391 1.024 0 1.414s1.024 0.391 1.414 0l4.293-4.293 4.293 4.293c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414z"
-            ></path>
-          </svg>
-        </div>
-      </a>
+      {/* Instead of <a href="#hero">, use a button */}
+      <button className="return-home" onClick={handleScrollToTop}>
+        <svg
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="#fafafa"
+            d="M17.707 10.293l-5-5c-0.391-0.391-1.024-0.391-1.414 0l-5 5c-0.391 0.391-0.391 1.024 0 1.414s1.024 0.391 1.414 0l4.293-4.293 4.293 4.293c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414zM17.707 17.293l-5-5c-0.391-0.391-1.024-0.391-1.414 0l-5 5c-0.391 0.391-0.391 1.024 0 1.414s1.024 0.391 1.414 0l4.293-4.293 4.293 4.293c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414z"
+          />
+        </svg>
+      </button>
       <div className="socials">
         <a
           href="https://github.com/Lrev47"
